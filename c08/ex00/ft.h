597711d6c4bcd527534cc43bade2a01c0f01f108
Debian <monandszy@tuta.io>
2025-07-11 11:15:46 +0200

    home work c08

---
+++ c08/ex00/ft.h
@@ -0,0 +1,12 @@
+#ifndef MY_HEADER_H
+#define MY_HEADER_H
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int	ft_strlen(char *str);
+int	ft_strcmp(char *s1, char *s2);
+
+#endif
---
+++ c08/ex01/ft_boolean.h
@@ -0,0 +1,15 @@
+typedef enum
+{
+	FALSE,
+	TRUE
+} t_bool;
+
+static inline int EVEN(int nbr)
+{
+	return (nbr % 2);
+}
+#define EVEN_MSG "I have an even number of arguments\n"
+#define ODD_MSG "I have an odd number of arguments\n"
+#define SUCCESS 0
+	
+
